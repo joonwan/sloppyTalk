f14d3ebbe51ef70f05ca5d721b7ac240fc364336
joonwan <joonwan1024@naver.com>
2023-11-06 19:58:05 +0900

    init project

---
+++ src/main/java/com/toro/sloppyTalk/SloppyTalkApplication.java
@@ -0,0 +1,13 @@
+package com.toro.sloppyTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloppyTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SloppyTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toro/sloppyTalk/SloppyTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toro.sloppyTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SloppyTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
